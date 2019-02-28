1c0ce1ee7b9c2b06eb5ad2e5c6aa77ba40568942
Feldrise <admin@feldrise.com>
2019-02-28 16:19:34 +0100

    Add project files.

---
+++ Calcea/Calcea/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calcea.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
